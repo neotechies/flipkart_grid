27d64b1eb38d1aa66b641a9d79b21742d89efdac
mbcse <mbcse50@gmail.com>
2021-09-11 21:46:21 +0530

    Url and template directory added along with readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Trust and Affluence signal extraction from social media data
+# CredSly: Social Credit Scoring System
+`Trust and Affluence signal extraction from social media data`
+
 `Flipkart_Grid 3.0`
+
+## Requirements
+- Python3 latest version recommended 
+- pipenv(For virtual Environment) `Run pip3 install pipenv`
+
+# How to Run Project
+- Clone project and run `pipenv install`
+- Run virtual environment `pipenv shell`
+- To run webApp
+  - `cd credsly`
+  - `python3 manage.py runserver`
--- credsly/credsly/settings.py
+++ credsly/credsly/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'credsly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
