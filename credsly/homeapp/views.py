4a88c79bd8a3f48cd8d808b3c1c6640ddc4e8346
mbcse <mbcse50@gmail.com>
2021-09-12 02:28:42 +0530

    Policy added

--- credsly/homeapp/views.py
+++ credsly/homeapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def privacy(request):
+    return render(request,'privacy_policy.html')    
