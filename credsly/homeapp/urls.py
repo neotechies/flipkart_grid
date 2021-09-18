77796440957ea3a38f92626517fa00d18fda5b25
mbcse <mbcse50@gmail.com>
2021-09-18 21:24:22 +0530

    Added dashboard, signin and signup pages

--- credsly/homeapp/urls.py
+++ credsly/homeapp/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('privacypolicy', views.privacy, name='Privacy-Policy'),
+    path('signin', views.sigin, name='sigin'),
+    path('signup', views.signup, name='signup'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- credsly/homeapp/views.py
+++ credsly/homeapp/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+def sigin(request):
+    return render(request,'sign-in.html')
+
+def signup(request):
+    return render(request,'sign-up.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
 def privacy(request):
     return render(request,'privacy_policy.html')    
