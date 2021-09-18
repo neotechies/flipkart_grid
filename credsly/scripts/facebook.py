7e19392358217e4225aba4156e79416ec3fbd5c8
mbcse <mbcse50@gmail.com>
2021-09-19 01:00:41 +0530

    Added extracting facebook zip file script

--- credsly/scripts/facebook.py
+++ credsly/scripts/facebook.py
@@ -1,1 +1,35 @@
-import requests
+from zipfile import ZipFile
+import os
+import shutil
+import json
+
+file_name = "../userDataUploads/fbdata.zip"  #testing
+
+
+
+def checkAndMakeDir(userID):
+    if(os.path.isdir("../userDataUploads/"+userID)):
+        if(os.path.isdir("../userDataUploads/"+userID+"/facebook")):
+            shutil.rmtree("../userDataUploads/"+userID+"/facebook")
+            os.mkdir("../userDataUploads/"+userID+"/facebook") 
+        else:
+            os.mkdir("../userDataUploads/"+userID+"/facebook")   
+    else:
+        os.makedirs("../userDataUploads/"+userID+"/facebook")
+                 
+def getFacebookData(zipName, userID):
+    checkAndMakeDir(userID)
+    
+    with ZipFile(file_name, 'r') as zip:
+        # extracting all the files
+        path="../userDataUploads/"+userID+"/facebook"
+        zip.extractall(path)
+
+
+def friendsCount(userID):
+    try:
+        
+    except Exception as e:
+        print("Error while Getting Friends Count for Facebook")
+
+
