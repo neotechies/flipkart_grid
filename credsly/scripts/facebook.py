ecacdf40384f36badbc995053ff96875c0687f18
mbcse <mbcse50@gmail.com>
2021-09-25 03:35:07 +0530

    All Functions Completed

--- credsly/scripts/facebook.py
+++ credsly/scripts/facebook.py
@@ -3,13 +3,23 @@ import os
 import shutil
 import json
 import boto3
+import glob
+from purgo_malum import client as client1
 
 ACCESS_KEY="AKIAQI6GMTIGYNVJZOAQ"
 SECRET_KEY="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"
-client = boto3.client(
+
+image_analysis_client = boto3.client(
     'rekognition',
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY,
+)
+
+text_analysis_client = boto3.client(
+    'comprehend',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    region_name = 'us-west-2'
 )
 
 
@@ -29,34 +39,31 @@ def checkAndMakeDir(userID):
     else:
         os.makedirs("../userDataUploads/"+userID+"/facebook")
                  
-def getFacebookData(zipName, userID):
-    checkAndMakeDir(userID)
-    path="../userDataUploads/"+userID+"/facebook"
-    with ZipFile(file_name, 'r') as zip:
-        # extracting all the files
-        
-        zip.extractall(path)
-  
-
 
 def getFriendsCount(path):
+    print("Starting Analysis Of Friend's Count...")
     try:
         f = open(path+"/friends_and_followers/friends.json",)
         data = json.load(f)
         return len(data['friends_v2'])
     except Exception as e:
-        print("Error while Getting Friends Count for Facebook"+e)
+        print("while Getting Friends Count for Facebook"+e)
         return None
+    finally:    
+        print("Analysis Of Friend's Count Completed")
+       
 
 def getReceivedFriendRequestsCount(path):
+    print("Starting Analysis Of Friend Request's...")
     try:
         f = open(path+"/friends_and_followers/friend_requests_received.json",)
         data = json.load(f)
         return len(data['received_requests_v2'])
     except Exception as e:
-        print("Error while Getting Friend Requests Count for Facebook"+e)
+        print("while Getting Friend Requests Count for Facebook"+e)
         return None
-
+    finally:    
+        print("Analysis Of Friend Request's Completed")
 # def getGroupsDetails(path):
 #     try:
 #         f = open(path+"/groups/your_group_membership_activity.json",)
@@ -66,43 +73,115 @@ def getReceivedFriendRequestsCount(path):
 #         print("Error while Getting Groups Details Count for Facebook"+e)
 #         return None
 
+
 def getPageFollowedList(path):
+    print("Starting Analysis Of Pages Followed...")
     try:
         f = open(path+"/pages/pages_you_follow.json",)
+        totalPages = 0
+        negativePages = 0
         data = json.load(f)
-        return data['pages_followed_v2'] # Has to got for textanalsys
+        for page_followed in data['pages_followed_v2']: # Has to got for textanalsys
+            try:
+                for page in page_followed['data']:
+                    totalPages+=1
+                    if(client1.contains_profanity(page['name'])):      #Checks for Bad words
+                        negativePages+=1 
+            except Exception as e:
+                print(e)            
+
+        return (negativePages,totalPages)
     except Exception as e:
-        print("Error "+e)
-        return None   
+        print(e)
+        return None 
+    finally:      
+        print("Analysis Of Pages Followed Completed")
 
 def getPageLikedList(path):
+    print("Starting Analysis Of Pages Liked...")
     try:
         f = open(path+"/pages/pages_you've_liked.json",)
         data = json.load(f)
-        return data['page_likes_v2'] # Has to got for textanalsys
+        totalPages = 0
+        negativePages = 0
+        for page in data['page_likes_v2']:
+            totalPages+=1
+            try:
+                if(client1.contains_profanity(page['name'])):      #Checks for Bad words
+                    negativePages+=1  
+            except Exception as e:
+                print(e)        
+        #return data['page_likes_v2']
+        # print(totalPages)
+        # print(negativePages)
+        return (negativePages,totalPages)
     except Exception as e:
-        print("Error "+e)
+        print(e)
         return None  
+    finally:    
+        print("Analysis Of Pages Liked Completed")
+
+def getPageList(path):
+    print("Starting Analysis Of Pages...")
+    n1,t1 = getPageLikedList(path)
+    n2,t2 = getPageFollowedList(path)  
+    print("Analysis Of Pages Completed")  
+    return (n1+n2)/(t1+t2)   
+    
+
 
 def getCommentsList(path):
+    print("Starting Analysis Of Comments...")
     try:
         f = open(path+"/comments_and_reactions/comments.json",)
         data = json.load(f)
-        return data['comments_v2'], len(data['comments_v2']) # Has to got for textanalsys
+        positiveComments=0
+        negativeComments=0
+        totalComments=0
+        for post in data['comments_v2']:
+            try:
+                for commentData in post['data']:
+                    print(commentData['comment']['comment'])
+                    totalComments+=1
+                    lang_response = text_analysis_client.detect_dominant_language(Text=commentData['comment']['comment'])
+                    languages = lang_response['Languages']
+                    lang_code = languages[0]['LanguageCode']
+                    response = text_analysis_client.detect_sentiment(Text=commentData['comment']['comment'], LanguageCode=lang_code)
+                    print(response)
+                    if(response['Sentiment']=='POSITIVE'):
+                        positiveComments+=1
+                    elif(response['Sentiment']=='NEGATIVE'):
+                        negativeComments+=1
+            except Exception as e:
+                print(e)
+
+        return positiveComments,negativeComments, totalComments
+
     except Exception as e:
-        print("Error "+e)
-        return None          
+        print(e)
+        return None  
+    finally:
+        print("Analysis Of Comments Completed")
+    
+                       
 
 def getPostsInteractionsCount(path):
+    ## Likes Count
+    print("Starting Analysis Of Posts...")
     try:
         f = open(path+"/comments_and_reactions/posts_and_comments.json",)
         data = json.load(f)
+        print(data)
         return len(data['reactions_v2'])
     except Exception as e:
-        print("Error "+e)
+        print(e)
         return None    
+    finally:
+        print("Analysis Of Posts Completed")
+
 
 def getProfileInfo(path):
+    print("Starting Analysis Of Profile...")
     try:
         f = open(path+"/profile_information/profile_information.json",)
         data = json.load(f)
@@ -126,24 +205,62 @@ def getProfileInfo(path):
                 education['type'] = edu['concentrations'][0]
             profileInfo['education_history'].append(education)
 
-        profileInfo['groups'] = []
+        totalGroups = 0
+        negativeGroups = 0
         for grp in data['groups']:
-            profileInfo['groups'].append(grp['name'])    
+            print(grp['name'])
+            totalGroups+=1
+            if(client1.contains_profanity(grp['name'])):      #Checks for Bad words
+                negativeGroups+=1
+        profileInfo['negativeGroupPercentage'] = negativeGroups/totalGroups     
 
         return profileInfo
+
     except Exception as e:
-        print("Error "+e)
+        print(e)
         return None
+    finally:
+        print("Analysis Of Profile Completed") 
 
+def getImageSentimentInfo(path):
+    print("Starting Analysis Of Images Uploaded...") 
+    try:
+        extensions = ['*.gif', '*.png', '*.jpg', '*.jpeg']
+        totalImages = 0
+        negativeImages = 0
+        for ext in extensions:
+            for filename in glob.iglob(path+ '/**/'+ext, recursive=True):
+                image = open(filename,'rb');
+                image_read = image.read()
+                totalImages+=1
+                response = image_analysis_client.detect_moderation_labels(
+                    Image={
+                        'Bytes': image_read,
+                    },
+                )
+                if(len(response['ModerationLabels'])>0):
+                    negativeImages+=1
+        return (negativeImages/totalImages)   
+    except Exception as e:
+        print(e)
+        return None
+    finally:
+        print("Analysis Of Images Completed")
 
-image = open(path+"/photos_and_videos/Coverphotos_2xJEHDkR2g/11407209_718802094913989_8372182250437187209_n_718802094913989.jpg", 'rb')
-image_read = image.read()
-
-response = client.detect_moderation_labels(
-    Image={
-        'Bytes': image_read,
-    },
-)
-
-print(response)
       
+
+def getFacebookData(zipName, userID):
+    # checkAndMakeDir(userID)
+    # path="../userDataUploads/"+userID+"/facebook"
+    # with ZipFile(file_name, 'r') as zip:
+    #     # extracting all the files
+    #     zip.extractall(path)
+    totalFriends = getFriendsCount(path)
+    totalFriendRequestRecieved = getReceivedFriendRequestsCount(path)
+    negativePageListPercentage = getPageList(path)
+    positiveComments,negativeComments, totalComments = getCommentsList(path)
+    totalReactionsOnPost = getPostsInteractionsCount(path) #Likes and Comments
+    totalNegativeImagesPercentage = getImageSentimentInfo(path)
+    profileInfo = getProfileInfo(path)
+
+getFacebookData("dadad","dada")  
